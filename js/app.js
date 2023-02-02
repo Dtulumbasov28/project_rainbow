3f86e8500aea882eba15e89bdcc0c1c4a97572ab
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2023-02-02 22:56:55 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -715,7 +715,6 @@
                 this.options.init ? this.initPopups() : null;
             }
             initPopups() {
-                this.popupLogging(`Проснулся`);
                 this.eventsPopup();
             }
             eventsPopup() {
@@ -6619,39 +6618,6 @@ PERFORMANCE OF THIS SOFTWARE.
             script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Abb5e2da36ced51eeb2559103acb5547f520b185b9b55ab4c3e49699dba36001e&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=false";
             document.getElementById("map__body").appendChild(script);
         }
-        window.addEventListener("DOMContentLoaded", (function() {
-            [].forEach.call(document.querySelectorAll(".input-tel"), (function(input) {
-                var keyCode;
-                function mask(event) {
-                    event.keyCode && (keyCode = event.keyCode);
-                    var pos = this.selectionStart;
-                    if (pos < 3) event.preventDefault();
-                    var matrix = "+7 (___) ___ ____", i = 0, def = matrix.replace(/\D/g, ""), val = this.value.replace(/\D/g, ""), new_value = matrix.replace(/[_\d]/g, (function(a) {
-                        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-                    }));
-                    i = new_value.indexOf("_");
-                    if (-1 != i) {
-                        i < 5 && (i = 3);
-                        new_value = new_value.slice(0, i);
-                    }
-                    var reg = matrix.substr(0, this.value.length).replace(/_+/g, (function(a) {
-                        return "\\d{1," + a.length + "}";
-                    })).replace(/[+()]/g, "\\$&");
-                    reg = new RegExp("^" + reg + "$");
-                    if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-                    if ("blur" == event.type && this.value.length < 5) this.value = "";
-                }
-                input.addEventListener("input", mask, false);
-                input.addEventListener("focus", mask, false);
-                input.addEventListener("blur", mask, false);
-                input.addEventListener("keydown", mask, false);
-            }));
-        }));
-        var script_checkbox = document.getElementById("popup-form__input-checkbox");
-        var btn_submit = document.querySelector(".popup-form__button");
-        script_checkbox.addEventListener("change", (() => {
-            if (script_checkbox.checked) btn_submit.removeAttribute("disabled"); else btn_submit.setAttribute("disabled", true);
-        }));
         window["FLS"] = true;
         isWebp();
         menuInit();
